306479c40ad08af9d4c2945278614f61f306a580
Ênio <146883699+Enio-Antonio@users.noreply.github.com>
2026-07-07 09:55:34 -0300

    Update random_numbers.zig

--- Random/random_numbers.zig
+++ Random/random_numbers.zig
@@ -4,7 +4,8 @@ pub fn main() !void {
     const seed: u64 = 123456789;
 
     // Non cryptographically secure
-    var genstrat = std.Random.DefaultPrng.init(seed);
-    const random_n = std.Random.intRangeAtMost(genstrat.random(), i32, 0, 100);
+    const genstrat = std.Random.DefaultPrng;
+    var generator = genstrat.init(seed);
+    const random_n = generator.random().intRangeAtMost(i32, 0, 10);
     std.debug.print("{}\n", .{random_n});
 }
